676b4dd85970b1142b5d3b263291e5d0a027ffb4
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2022-09-10 10:47:53 -0300

    Conexão com a API - Criando hook para o resumo

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,38 +1,11 @@
-import { useContext } from 'react'
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
-import { TransactionContext } from '../../contexts/TransactionsContext'
 
 import { SummaryCard, SummaryContainer } from './styles'
 import { priceFormatter } from '../../utils/formatter'
+import { useSummary } from '../../hooks/useSummary'
 
 export function Summary() {
-  const { transactions } = useContext(TransactionContext)
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      switch (transaction.type) {
-        case 'income':
-          acc.income += transaction.price
-          acc.total += transaction.price
-          break
-        case 'outcome':
-          acc.outcome += transaction.price
-          acc.total -= transaction.price
-          break
-        default:
-          break
-      }
-
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
-
-  console.log(summary)
+  const summary = useSummary()
 
   return (
     <SummaryContainer>
